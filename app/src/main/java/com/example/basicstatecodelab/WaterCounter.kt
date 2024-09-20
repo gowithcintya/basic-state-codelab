298f0a0f2c24d0c0e7f23b3267cbb6b2214ad825
gowithcintya <cintyagomes@id.uff.br>
2024-09-19 22:48:36 -0300

    [FEATURE] - Setting remember in composition

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,12 +1,14 @@
 package com.example.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -18,14 +20,29 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         var count by remember { mutableIntStateOf(0) }
 
         if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 minute walk today?",
+                    onClose = { showTask = false }
+                )
+            }
             Text(text = "You've had $count glasses.")
         }
-        Button(
-            onClick = { count++ },
-            enabled = count < 10,
-            modifier = Modifier.padding(top = 8.dp)
-        ) {
-            Text("Add one")
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(
+                onClick = { count++ },
+                enabled = count < 10,
+                modifier = Modifier.padding(top = 8.dp)
+            ) {
+                Text("Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                modifier = Modifier.padding(top = 8.dp)
+            ) {
+                Text("Clear water count")
+            }
         }
     }
 }
