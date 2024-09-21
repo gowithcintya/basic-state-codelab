d6e576794595aefe346f3d97a126f64b3b135d03
gowithcintya <cintyagomes@id.uff.br>
2024-09-20 21:21:48 -0300

    [FEATURE] - State hoisting

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -14,16 +14,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(16.dp)) {
-        var count by rememberSaveable { mutableIntStateOf(0) }
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableIntStateOf(0) }
+    StatelessCounter(count, { count++ }, modifier)
+}
 
+@Composable
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.padding(16.dp)) {
         if (count > 0) {
             Text(text = "You've had $count glasses.")
         }
         Row(Modifier.padding(top = 8.dp)) {
             Button(
-                onClick = { count++ },
+                onClick = { onIncrement() },
                 enabled = count < 10,
                 modifier = Modifier.padding(top = 8.dp)
             ) {
